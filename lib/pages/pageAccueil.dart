9b455244d75b408a0b39435259f9f76e55ebda13
TheoLeao <theoleaoboudier@gmail.com>
2020-01-20 17:48:43 +0100

    Avancement sur le design "Ma Carte":)

--- lib/pages/pageAccueil.dart
+++ lib/pages/pageAccueil.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import '../themes/color.dart';
 
 //Class permettant d'aligner horizontalement un text
-class CenterHorizontal extends StatelessWidget {   
-    CenterHorizontal(this.child);   
-    final Widget child;
-    @override   
-   Widget build(BuildContext context) => 
-        Row(mainAxisAlignment: MainAxisAlignment.center,children: [child]); 
+class CenterHorizontal extends StatelessWidget {
+  CenterHorizontal(this.child);
+  final Widget child;
+  @override
+  Widget build(BuildContext context) =>
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [child]);
 }
 
 class PageAccueil extends StatelessWidget {
@@ -21,6 +21,7 @@ class PageAccueil extends StatelessWidget {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
+                      //Padding du text "Ma Cave"
                       Padding(
                           padding: EdgeInsets.all(12.0),
                           child: Align(
@@ -29,36 +30,47 @@ class PageAccueil extends StatelessWidget {
                                 style: TextStyle(
                                     fontSize: 22, fontWeight: FontWeight.bold)),
                           )),
+                      //Align du container jaune
                       Align(
                         alignment: Alignment.center,
                         child: Container(
                             height: 300,
                             width: 370,
                             color: SecondaryColor,
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                  
-                                  children: <Widget>[
-                                  Container(
-                                        width: 125.0,
-                                        height: 125.0,
+                            child: Column(children: <Widget>[
+                              Stack(
+                                children: <Widget>[
+                                  Align(
+                                      alignment: AlignmentDirectional(-0.8, 0),
+                                      child: //Container cercle blanc
+                                          Container(
+                                        width: 120.0,
+                                        height: 120.0,
                                         decoration: new BoxDecoration(
                                           color: Colors.white,
                                           shape: BoxShape.circle,
                                         ),
+                                        //Colonne à l'intérieur du cercle
                                         child: Column(
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
                                           children: <Widget>[
-                                            CenterHorizontal(Text("34",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold))),
-                                            CenterHorizontal(Text("bouteilles",style: TextStyle(fontSize: 20)))
+                                            //Texts à l'interieur de la colonne dans le cercle
+                                            CenterHorizontal(Text("34",
+                                                style: TextStyle(
+                                                    fontSize: 40,
+                                                    fontWeight:
+                                                        FontWeight.bold))),
+                                            CenterHorizontal(Text("bouteilles",
+                                                style: TextStyle(fontSize: 20)))
                                           ],
                                         ),
-                                      )
-                                ])
-                              ],
-                            )),
+                                      ))
+                                ],
+                              )
+                            ])),
                       )
                     ]))));
   }
--- lib/pages/pageMetsVins.dart
+++ lib/pages/pageMetsVins.dart
@@ -1,10 +1,60 @@
+import 'package:cavi/themes/color.dart';
 import 'package:flutter/material.dart';
 
+//Class permettant d'aligner horizontalement un text
+class CenterHorizontal extends StatelessWidget {
+  CenterHorizontal(this.child);
+  final Widget child;
+  @override
+  Widget build(BuildContext context) =>
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [child]);
+}
 class PageMetsVins extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Page Mets Vins", textScaleFactor: 2.0,)
-    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
+      body: Stack(
+        children: <Widget>[
+          //Align du tite "Ma Cave"
+          Align(
+            alignment: AlignmentDirectional(0, -0.92),
+            child: Text("Ma Cave",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))
+          ),
+          //Align du rectangle jaune
+          Align(
+            alignment: AlignmentDirectional(0, -0.68),
+            child: Container(
+            alignment: Alignment.center,
+            width: 350,
+            height: 250,
+            color: SecondaryColor,
+          ),
+          ),
+          //Align du cercle blanc
+          Align(
+            alignment: AlignmentDirectional(-0.66, -0.69),
+            child: Container(
+              height: 105,
+              width: 105,
+              decoration: new BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle
+              ),
+            ),
+          ),
+          //Align du texte "34"
+          Align(
+            alignment: AlignmentDirectional(-0.55, -0.68),
+              child: Text("34", style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold) )
+          ),
+          Align(
+            alignment: AlignmentDirectional(-0.61, -0.58),
+              child: Text("bouteilles", style: TextStyle(fontSize: 20) )
+          )
+        ],
+      ),
+    ));
   }
 }
